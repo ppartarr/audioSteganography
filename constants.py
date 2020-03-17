f18ae0cc383cac1deef6189c6d4c501bf94c6abb
streambinder <posta@davidepucci.it>
2020-03-17 23:59:09 +0100

    Tune parsing and model parameters up

--- constants.py
+++ constants.py
@@ -1,13 +1,12 @@
-epochs = 100
-batch_size = 32
+epochs = 1000
 num_samples = 1000
+batch_size = 32
 sample_rate = 16000
 
 # STFT transform
-num_mel_filters = 256
 lower_edge_hertz = 20.0
 upper_edge_hertz = 8000.0
-frame_length = 1024
-frame_step = 512
-fft_length = 1024
+frame_length = 160
+frame_step = 40
 num_fft = 2048
+num_mel_filters = 512
