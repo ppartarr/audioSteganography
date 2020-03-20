7eef692c7191569dda6c43b2e183ae3dfc8a5e65
Philippe Partarrieu <ppartarr@outlook.com>
2020-03-20 23:31:57 +0100

    Cut train & test data
    Former-commit-id: e4f17369440e6ad42410a947015db90ba6b64a39

--- fix_length.sh
+++ fix_length.sh
@@ -2,13 +2,26 @@
 
 length="02.50"
 path_data="data"
-path_src="TRAIN"
-path_dst="TRAIN_CUT"
+train_train_path_src="TRAIN"
+train_train_path_dst="TRAIN_CUT"
 
-samples="$(find "${path_data}/${path_src}" -type f -name '*.WAV.wav' | xargs)"
+test_path_src="TEST"
+test_path_dst="TEST_CUT"
 
-for src_wav_file in ${samples}; do
-    dst_wav_file="${src_wav_file/${path_src}/${path_dst}}"
+train_samples="$(find "${path_data}/${train_path_src}" -type f -name '*.WAV.wav' | xargs)"
+test_samples="$(find "${path_data}/${train_path_src}" -type f -name '*.WAV.wav' | xargs)"
+
+for src_wav_file in ${train_samples}; do
+    dst_wav_file="${src_wav_file/${train_path_src}/${train_path_dst}}"
+    mkdir -p "$(dirname "${dst_wav_file}")"
+
+    echo "Adapting ${src_wav_file} to ${dst_wav_file}"
+    ffmpeg -y -stream_loop -1 -i "${src_wav_file}" \
+        -ss 00:00:00.00 -to 00:00:${length} "${dst_wav_file}" > /dev/null 2>&1
+done
+
+for src_wav_file in ${test_samples}; do
+    dst_wav_file="${src_wav_file/${test_path_src}/${test_path_dst}}"
     mkdir -p "$(dirname "${dst_wav_file}")"
 
     echo "Adapting ${src_wav_file} to ${dst_wav_file}"
