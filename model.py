4264386ca09e4bafd26b81cc5f9a8443e9f9ec86
Philippe Partarrieu <ppartarr@outlook.com>
2020-03-25 12:19:02 +0100

    Add loss & layers back in
    Former-commit-id: 4d1edcdc7cd50837fbe442871db881d4abee6a65

--- model.py
+++ model.py
@@ -12,6 +12,10 @@ def steg_model(input_shape, pretrain=False):
         "hide_conv_f": losses.mean_squared_error,
         "revl_conv_f": losses.mean_squared_error,
     }
+    lossWeights = {
+        "hide_conv_f": 1.0,
+        "revl_conv_f": 0.8
+    }
 
     # Inputs
     secret = Input(shape=input_shape, name='secret')
@@ -88,9 +92,31 @@ def steg_model(input_shape, pretrain=False):
     hconv_5x5 = Conv2D(64, kernel_size=3, padding="same",
                        activation='relu', name='hide_conv5x5_4')(hconv_5x5)
 
-    hconcat_1 = concatenate(
-        [hconv_3x3, hconv_4x4, hconv_5x5], axis=3, name="hide_concat_2")
+    hconv_6x6 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='hide_conv6x6_1')(hconcat_h)
+    hconv_6x6 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='hide_conv6x6_2')(hconv_6x6)
+    hconv_6x6 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='hide_conv6x6_3')(hconv_6x6)
+    hconv_6x6 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='hide_conv6x6_4')(hconv_6x6)
 
+    hconv_7x7 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='hide_conv7x7_1')(hconcat_h)
+    hconv_7x7 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='hide_conv7x7_2')(hconv_7x7)
+    hconv_7x7 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='hide_conv7x7_3')(hconv_7x7)
+    hconv_7x7 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='hide_conv7x7_4')(hconv_7x7)
+
+    hconcat_1 = concatenate(
+        [hconv_7x7, hconv_6x6, hconv_5x5, hconv_3x3, hconv_4x4, hconv_5x5], axis=3, name="hide_concat_2")
+
+    hconv_7x7 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='hide_conv7x7_f')(hconcat_1)
+    hconv_6x6 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='hide_conv6x6_f')(hconcat_1)
     hconv_5x5 = Conv2D(64, kernel_size=3, padding="same",
                        activation='relu', name='hide_conv5x5_f')(hconcat_1)
     hconv_4x4 = Conv2D(64, kernel_size=3, padding="same",
@@ -159,9 +185,25 @@ def steg_model(input_shape, pretrain=False):
     rconv_8x8 = Conv2D(64, kernel_size=3, padding="same",
                        activation='relu', name='revl_conv8x8_4')(rconv_8x8)
 
+    rconv_9x9 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='revl_conv9x9_1')(cover_pred)
+    rconv_9x9 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='revl_conv9x9_2')(rconv_9x9)
+    rconv_9x9 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='revl_conv9x9_3')(rconv_9x9)
+    rconv_9x9 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='revl_conv9x9_4')(rconv_9x9)
     rconcat_1 = concatenate(
-        [rconv_3x3, rconv_4x4, rconv_5x5], axis=3, name="revl_concat_1")
+        [rconv_3x3, rconv_4x4, rconv_5x5, rconv_6x6, rconv_7x7, rconv_8x8, rconv_9x9], axis=3, name="revl_concat_1")
 
+    rconv_9x9 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='revl_conv9x9_f')(rconcat_1)
+    rconv_8x8 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='revl_conv8x8_f')(rconcat_1)
+    rconv_7x7 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='revl_conv7x7_f')(rconcat_1)
+    rconv_6x6 = Conv2D(64, kernel_size=3, padding="same",
+                       activation='relu', name='revl_conv6x6_f')(rconcat_1)
     rconv_5x5 = Conv2D(64, kernel_size=3, padding="same",
                        activation='relu', name='revl_conv5x5_f')(rconcat_1)
     rconv_4x4 = Conv2D(64, kernel_size=3, padding="same",
@@ -170,7 +212,7 @@ def steg_model(input_shape, pretrain=False):
                        activation='relu', name='revl_conv3x3_f')(rconcat_1)
 
     rconcat_f1 = concatenate(
-        [rconv_5x5, rconv_4x4, rconv_3x3], axis=3, name="revl_concat_2")
+        [rconv_8x8, rconv_7x7, rconv_6x6, rconv_5x5, rconv_4x4, rconv_3x3], axis=3, name="revl_concat_2")
 
     secret_pred = Conv2D(input_shape[2], kernel_size=1, padding="same",
                          name='revl_conv_f')(rconcat_f1)
@@ -178,6 +220,6 @@ def steg_model(input_shape, pretrain=False):
     model = Model(inputs=[secret, cover], outputs=[secret_pred, cover_pred])
 
     # Compile model
-    model.compile(optimizer='adam', loss=lossFns)
+    model.compile(optimizer='adam', loss=lossFns, loss_weights=lossWeights)
 
     return model
