525855899d22132af6c039742d8d59835e424b41
Philippe Partarrieu <ppartarr@outlook.com>
2020-03-28 14:02:53 +0100

    Make encoder

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-epochs = 100
+epochs = 200
 num_samples = 6300
 batch_size = 64
 sample_rate = 16000
@@ -10,4 +10,4 @@ upper_edge_hertz = 8000.0
 frame_length = 160
 frame_step = frame_length // 4
 num_mel_filters = 64
-n_fft = 1024
+n_fft = 512
